f575730243d55b379796a6695fc261b27f3d134b
Cameron Chamberlain <hi@camjc.com>
2018-02-26 21:06:40 +1100

    [minor] use json to drive data

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,34 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+const path = require('path');
+const data = require('./src/resume.json');
+// const _ = require('lodash');
+
+exports.createPages = ({ boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+
+  const overviewTemplate = path.resolve(`src/pages/overview.js`);
+  createPage({
+    path: '/',
+    component: overviewTemplate,
+    context: data
+  });
+
+  // const employerTemplate = path.resolve(`src/pages/employer.js`);
+  // _(data.employment.history)
+  //   .groupBy('employer')
+  //   .map(({ page }) => {
+  //     const path = page;
+  //
+  //     createPage({
+  //       path,
+  //       component: employerTemplate,
+  //
+  //       // Send additional data to page from JSON (or query inside template)
+  //       context: {
+  //         path
+  //       }
+  //     });
+  // });
+};
---
+++ src/pages/overview.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import _ from 'lodash'
+
+const IndexPage = ({pathContext: data}) =>{
+  return <div>
+    <h1>Résumé</h1>
+    <h2>{data.info.label}</h2>
+    <p>{data.info.brief}</p>
+
+    <section>
+      <h2>Employment</h2>
+      {_(data.employment.history)
+        .groupBy('employer')
+        .map((employerData, employer) => {
+          return <div key={employer}>
+            <h3>{employer}</h3>
+            {_.map(employerData, (role) =>
+              <div key={role.position}>
+                <h4>{role.position}</h4>
+                <ul>
+                  {_.map(role.highlights, (highlight) => <li key={highlight}>
+                    {highlight}
+                  </li>)}
+                </ul>
+              </div>
+            )}
+          </div>
+        })
+        .value()}
+    </section>
+  </div>
+};
+
+export default IndexPage
